ca3e4ecdc03a93f5b83f7daee1112d7d8730d347
jhs <skc05096@naver.com>
2025-11-23 17:20:36 +0900

    웹소캣 프로젝트생성

---
+++ spring-chat-matching/src/main/java/com/chatmatchingservice/springchatmatching/SpringChatMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.chatmatchingservice.springchatmatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatMatchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChatMatchingApplication.class, args);
+    }
+
+}
---
+++ spring-chat-matching/src/test/java/com/chatmatchingservice/springchatmatching/SpringChatMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatmatchingservice.springchatmatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatMatchingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
